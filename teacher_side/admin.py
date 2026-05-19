a77f0b0f1dfe0b243b361707d8eb30001355a6e4
Abel Gabor <abel@abel.hu>
2026-05-19 23:37:55 +0200

    fix: missing RematchAuditLog import in teacher_side/admin.py

--- teacher_side/admin.py
+++ teacher_side/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TeamNameTemplate, CSVGeneration
+from .models import TeamNameTemplate, CSVGeneration, RematchAuditLog
 
 
 @admin.register(TeamNameTemplate)
